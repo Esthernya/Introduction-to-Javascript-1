b24b09c5fecb2b0f7f5eae64ecabdecdec1bc02b
Esthernya <104891692+Esthernya@users.noreply.github.com>
2022-06-24 13:57:30 +0100

    New commit

---
+++ Javascript/script.js
@@ -0,0 +1,3 @@
+alert("My name is Esther Nya \nI am from Nigeria\nI have a height of 5ft10")
+document.write("My name is Esther Nya \nI am from Nigeria\nI have a height of 5ft10")
+console.log("My name is Esther Nya \nI am from Nigeria\nI have a height of 5ft10")
